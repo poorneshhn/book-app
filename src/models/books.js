3c67fcb49a6848f133a6465ea4885a444d0bfa8b
unknown <poorneshhn3@gmail.com>
2021-08-29 18:29:17 +0530

    added book routes and models

--- src/models/books.js
+++ src/models/books.js
@@ -2,13 +2,38 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const bookSchema = new mongoose.Schema({
-    bookname: {
+    title: {
         type: String,
         required: true,
         validate(value) {
             if(value.trim == "") throw new Error("name can not be Space!");
-        }
-    }
+        }},
+    description: {
+        type: String,
+        required: true
+        },
+    publishDate: {
+        type: Date,
+        required: true
+        },
+    pageCount: {
+        type: Number,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    coverImageName: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Author"
+    }    
 })
 
 const Book = mongoose.model("Book", bookSchema);
--- src/routers/bookRouter.js
+++ src/routers/bookRouter.js
@@ -1,27 +1,49 @@
 const express = require("express");
 const Book = require("../models/books");
+const Author = require("../models/authors");
 
 const bookRouter = new express.Router();
 
 bookRouter.get("/", (req, res) => {
     res.status(200).render("bookviews/index", {
-        title: "Books Book-App"
-    });
+        title: "Books - Book-App"
+    })
 })
 
-bookRouter.post("/newbook", async (req, res) => {
+bookRouter.post("/", async (req, res) => {
     try {
-        console.log(req.body);
-        const book = await new Book({
-            bookname: req.body.bookname
-        });
-        await book.save();
-        res.status(200).redirect("/");
+        
+        let value = req.body;
+
+        console.log(value);
+    
+    const book = await new Book({
+        title: value.title,
+        description: value.description,
+        publishDate: value.publishDate,
+        pageCount: value.pageCount,
+        createdAt: value.createdDate,
+        coverImageName: value.coverImageTitle,
+        author: value.author 
+    })
+
+    await book.save();
+
+    res.status(200).redirect("/books");
+
     } catch (error) {
-        console.log(error);
+        res.status(500).send(error);    
     }
-    console.log(req.body);
-    //res.send("Create");
+})
+
+bookRouter.get("/new", async (req, res) => {
+
+    const authors = await Author.find({});
+    res.status(200).render("bookviews/new", {
+        title: "New Book - Book-App",
+        authors: authors
+    })
+    
 })
 
 module.exports = bookRouter;
